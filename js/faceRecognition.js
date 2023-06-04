18d03e307ec329d76e799b455d4503b1d7987ae4
FGL200 <2020-100627@rtu.edu.ph>
2023-06-04 22:57:47 +0800

    OCR

--- js/faceRecognition.js
+++ js/faceRecognition.js
@@ -6,7 +6,8 @@ let imgUploadProperties;
 let imgUploadReady = false;
 let faceDetedted = false;
 
-let finalResult = {complete: false, result:null};
+const IDResult = {complete: false, result:null};
+let IDIndex = -1;
 
 Promise.all([
     faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
@@ -14,10 +15,18 @@ Promise.all([
     faceapi.nets.ssdMobilenetv1.loadFromUri('./models'),
     faceapi.nets.tinyFaceDetector.loadFromUri('./models')
 ]).then(()=>{
+    localStorage.clear();
+
     const element = document.createElement("p");
     element.setAttribute("id", "id-validation-result")
     element.style.textAlign = "center";
     document.querySelector(".step1").appendChild(element);
+    
+    const hiddenInput = document.createElement("input");
+    hiddenInput.setAttribute("id", "id-ocr-result")
+    hiddenInput.setAttribute("type", "hidden")
+    hiddenInput.setAttribute("value", "{}")
+    document.querySelector(".main").appendChild(hiddenInput);
 }).catch(err=>{alert("Error: " + err);});
 
 function loadImgAsPromise(img){
@@ -33,30 +42,60 @@ function loadImgAsPromise(img){
 }
 
 imgUpload.addEventListener("change", async (e)=>{
-    const image = await faceapi.bufferToImage(imgUpload.files[0]);
+    const ImageStructure = [];
+
+    for(let index = 0; index < imgUpload.files.length; index++){
+        const image = await faceapi.bufferToImage(imgUpload.files[index]);
+        const detection = await faceapi.detectAllFaces(image);
+        
+        if(detection.length > 0)
+            ImageStructure.push({hasFace: true});
+        else
+            ImageStructure.push({hasFace: false});
+    }
+    
+    if(ImageStructure.length < 1){
+        imgUpload.value = "";
+        return;
+    }else{
+        let counter = 0;
+        for(let i = 0; i < ImageStructure.length; i++){
+            if(ImageStructure[i].hasFace){
+                IDIndex = i;
+                counter ++;
+            }
+        }
+        if(counter != 1 || IDIndex == -1){
+            alert("Invalid ID, please upload another ID (front and back)")
+            imgUpload.value = "";
+            return;
+        }
+    }
+
+    const image = await faceapi.bufferToImage(imgUpload.files[IDIndex]);
     const detection = await faceapi.detectAllFaces(image);
 
     const element = document.querySelector("#id-validation-result");
 
     if(detection.length > 0){
-        element.innerHTML = "Reading your ID...";
+       element.innerHTML = "Reading your ID...";
 
-        imgUploadProperties = await loadImgAsPromise(image);
-        alert("You may now start capturing!");
+       imgUploadProperties = await loadImgAsPromise(image);
+       alert("You may now start capturing!");
 
-        element.style.color = "#29b198";
-        element.style.fontWeight = "bold"
-        element.innerHTML = "ID uploaded!";
+       element.style.color = "#29b198";
+       element.style.fontWeight = "bold"
+       element.innerHTML = "ID uploaded!";
 
-        imgUpload.style.display = "none"
-        document.querySelector("#reUpload").style.display = "block"
-        imgUploadReady = true;
-        startVideo();
+       imgUpload.style.display = "none"
+       document.querySelector("#reUpload").style.display = "block"
+       imgUploadReady = true;
+       startVideo();
     }else{
-        element.style.color = "red";
-        element.innerHTML = "Invalid ID";
-        alert("Invalid ID, please reupload another ID");
-        imgUploadReady = false;
+       element.style.color = "red";
+       element.innerHTML = "Invalid ID";
+       alert("Invalid ID, please reupload another ID");
+       imgUploadReady = false;
     }
 });
 
@@ -102,7 +141,7 @@ async function faceDetectionHandler(){
     var faceMatcher = new faceapi.FaceMatcher(imgUploadProperties);
     const displaySizeOfVideo = { width: 320, height: 240 };
 
-    if(!finalResult.complete){
+    if(!IDResult.complete){
         const detections = await faceapi
             .detectAllFaces(video)
             .withFaceLandmarks()
@@ -115,8 +154,8 @@ async function faceDetectionHandler(){
 
         results.forEach((result) => {
             if(String(result.label) === String("faceFound")){
-                finalResult.complete = true;
-                finalResult.result = result;
+                IDResult.complete = true    ;
+                IDResult.result = result;
             }else{
                 alert("Face did not match from ID, Please recapture or upload another ID");
                 faceDetedted = false;
@@ -124,8 +163,44 @@ async function faceDetectionHandler(){
         });
     }
     
-    if(finalResult.complete){
+    if(IDResult.complete){
         alert("Face Recognized from ID!");
-        console.log(finalResult);
+        btnCapture.disabled = true;
+        SetIDValue();
+    }else{
+        alert("Face did not match from ID, Please recapture or upload another ID");
+    }
+}
+
+function SetIDValue(){
+    const OCR_VAL = [];
+    for(let i = 0; i < imgUpload.files.length; i++){
+        const formData = new FormData();
+        formData.append('image', imgUpload.files[i]);
+
+        fetch('https://api.api-ninjas.com/v1/imagetotext', {
+            method: 'POST',
+            headers: { 'x-api-key': 'Bsb6tj0moXhWqmCu01A7FQ==AVEYlPiR3kHQlvtr' },
+            body: formData
+        })
+            .then(response => response.json())
+            .then(result => {
+                localStorage.setItem("OCR-" + i, JSON.stringify(result));
+                console.log(localStorage.getItem("OCR-" + i));
+                OCR_VAL.push(result);
+                return i;
+            })
+            .then((index)=>{
+                console.log("i=" + index);
+                console.log((parseInt(index) === parseInt(imgUpload.files.length - 1)));
+                if(parseInt(index) === parseInt(imgUpload.files.length - 1)){
+                    document.getElementById("id-ocr-result").value = JSON.stringify(OCR_VAL);
+                    console.log(OCR_VAL)
+                }
+            })
+            .catch(error => {
+                alert(error);
+            });
+
     }
 }
