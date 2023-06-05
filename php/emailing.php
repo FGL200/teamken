c84594f14e2e422234a48ed5cc7c45cebbd24e5f
CHARLES IGNACIO <charlesignacio6@gmail.com>
2023-06-05 23:40:04 +0800

    send confirmation email

--- php/emailing.php
+++ php/emailing.php
@@ -11,7 +11,7 @@ require '../vendor/autoload.php';
 
 // EMAIL CONFIRMATION
 
-function admin_verify($name, $email, $verify_token) {
+function synergy($surname, $firstname, $middlename, $email, $phone, $reference) {
     $mail = new PHPMailer(true);
 
     $mail -> isSMTP();
@@ -29,10 +29,83 @@ function admin_verify($name, $email, $verify_token) {
     $mail -> isHTML(true);
     $mail -> Subject = 'Email Verification from CDSSA';
 
-    $email_template = "";
+    $email_template = "
+        <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css' rel='stylesheet'>
+        Good day, $firstname $middlename $surname! <br><br>
+
+        This email serves as confirmation that we have received your application for Converge. <br>
+        Click the link below if you confirm, else disregard this email. <br><br>
+
+        Thank you!
+
+        <center><a class='btn btn-primary' href='http://localhost/teamken/php/confirmation.php?token=$reference'> Confirm Application </a></center>
+    ";
 
     $mail -> Body = $email_template;
     $mail -> send();
+}
+
+
+if(isset($_POST['apply_btn'])) {
+    $surname = $_POST['surname'];
+    $firstname = $_POST['fname'];
+    $middlename = $_POST['mname'];
+    $birthday = $_POST['bday'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+
+    $reference = md5(rand(15));
+
+    $valphone = (strlen($phone) < 11 || strlen($phone) > 11);
+
+    $check_email_query = "SELECT email FROM users WHERE email='$email' LIMIT 1";
+    $check_email_query_run = mysqli_query($con, $check_email_query);
+
+    $check_phone_query = "SELECT phone FROM users WHERE phone='$phone' LIMIT 1";
+    $check_phone_query_run = mysqli_query($con, $check_phone_query);
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $_SESSION['status'] = "Invalid Email Address.";
+        $_SESSION['status_code'] = "error";
+        header("Location: emailtester.php");
+    }
+
+    else if ($valphone) {
+        $_SESSION['status'] = "Invalid Phone Number.";
+        $_SESSION['status_code'] = "error";
+        header("Location: emailtester.php");
+    }
+
+    else if (mysqli_num_rows($check_email_query_run) > 0) {
+        $_SESSION['status'] = "Email Address has already exists.";
+        $_SESSION['status_code'] = "error";
+        header("Location: emailtester.php");
+    }
+
+    else if (mysqli_num_rows($check_phone_query_run) > 0) {
+        $_SESSION['status'] = "Phone Number has already exists.";
+        $_SESSION['status_code'] = "error";
+        header("Location: emailtester.php");
+    }
+
+    else {
+        $query = "INSERT INTO users (surname, firstname, middlename, birthday, address, email, phone, reference) VALUES ('$surname', '$firstname', '$middlename', '$birthday', '$address', '$email', '$phone', '$reference')";
+        $query_run = mysqli_query($con, $query);
+
+        if ($query_run) {
+            synergy($surname, $firstname, $middlename, $email, $phone, $reference);
+            $_SESSION['status'] = "Application Successful. Check you Email for confirmation.";
+            $_SESSION['status_code'] = "success";
+            header("Location: emailtester.php");
+        }
+
+        else {
+            $_SESSION['status'] = "Something went wrong.";
+            $_SESSION['status_code'] = "error";
+            header("Location: emailtester.php");
+        }
+    }
 }
 
 ?>
