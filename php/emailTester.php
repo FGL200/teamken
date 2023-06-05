d02ca181dfed49a8c90823b4b1feca24b2b561d7
CHARLES IGNACIO <charlesignacio6@gmail.com>
2023-06-05 23:40:19 +0800

    temporary info page only

--- php/emailTester.php
+++ php/emailTester.php
@@ -1,7 +1,6 @@
 <?php
 
 session_start();
-include('alert.php');
 
 ?>
 
@@ -14,28 +13,24 @@ include('alert.php');
             <div class="col-md-6">
                 <div class="card shadow">
                     <div class="card-header">
-                        <h5>CDSSA Internship</h5>
+                        <h5>User Information</h5>
                     </div>
                     <div class="card-body">
-                        <form action="../backend/code.php" method="POST">
+                        <form action="emailing.php" method="POST">
                         <div class="form-group mb-3">
-                            <center><h4>Internship Application</h4></center> <br>
-                            <h5>Applicant Information</h5>
+                            <center><h4>Applicant Information</h4></center> <br>
 
-                            <input type="text" name="name" class="form-control" placeholder="Name: (Last Name, First Name, M.I.)    " required>
-                            <input type="text" name="email" class="form-control" placeholder="Email:" required>
-                            <input type="number" name="age" class="form-control" placeholder="Age:" required>
-                            <textarea type="text" name="address" class="form-control" placeholder="Home Address:" required></textarea>
-                            <input type="text" name="institution" class="form-control" placeholder="Institution:" required>
-                            <input type="text" name="college" class="form-control" placeholder="College:" required>
-                            <input type="text" name="department" class="form-control" placeholder="Department:" required>
-                            <input type="text" name="degree" class="form-control" placeholder="Degree Program:" required><br>
-                            <label>Attach your Cover Letter and Curriculum Vitae</label><br>
-                            <input type="file">
+                            <input type="text" name="sname" class="form-control" placeholder="Surname:" required>
+                            <input type="text" name="fname" class="form-control" placeholder="First Name:" required>
+                            <input type="text" name="mname" class="form-control" placeholder="Middle Name:" required>
+                            <input type="text" name="bday" class="form-control" placeholder="Birthdate:" required>
+                            <textarea type="text" name="address" class="form-control" placeholder="Address:" required></textarea><br>
+
+                            <input type="email" name="email" class="form-control" placeholder="Email: name@example.com" required>
+                            <input type="number" name="phone" class="form-control" placeholder="Phone: 09xxxxxxxxxx" required>
                         </div>
                         <div class="form-group">
-                            <a href="<?php echo $_SERVER['HTTP_REFERER']; ?>" class="btn btn-primary">Back</a>
-                            <button type="submit" name="intern_btn" class="btn btn-primary">Submit Application</button>
+                            <button type="submit" name="apply_btn" class="btn btn-primary">Submit Application</button>
                         </div>
                         </form>
                     </div>
@@ -48,6 +43,6 @@ include('alert.php');
 
 <?php
 
-include('../includes/scripts.php');
+include('alert.php');
 
 ?>
