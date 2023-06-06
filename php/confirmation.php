5ca79ff97442fd7e5b0a512875b260ed60f2dbe2
CHARLES IGNACIO <charlesignacio6@gmail.com>
2023-06-06 22:03:03 +0800

    confirmation from user

--- php/confirmation.php
+++ php/confirmation.php
@@ -5,15 +5,15 @@ include('dbcon.php');
 
 if (isset($_GET['token'])) {
     $token = $_GET['token'];
-    $verify_query = "SELECT confirmation, confirmation FROM users WHERE confirmation='$token' LIMIT 1";
+    $verify_query = "SELECT confirmation_token, confirmation FROM users WHERE confirmation_token='$token' LIMIT 1";
     $verify_query_run = mysqli_query($con, $verify_query);
 
     if (mysqli_num_rows($verify_query_run) > 0) {
         $row = mysqli_fetch_array($verify_query_run);
         
         if ($row['confirmation'] == 0) {
-            $clicked_token = $row['confirmation'];
-            $update_query = "UPDATE users SET confirmation='1' WHERE confirmation='$clicked_token' LIMIT 1";
+            $clicked_token = $row['confirmation_token'];
+            $update_query = "UPDATE users SET confirmation='1' WHERE confirmation_token='$clicked_token' LIMIT 1";
             $update_query_run = mysqli_query($con, $update_query);
 
             if ($update_query_run) {
