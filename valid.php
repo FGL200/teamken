5e486b7de7d2329514e01d47b0cd2300186978e5
CHARLES IGNACIO <charlesignacio6@gmail.com>
2023-06-07 11:48:58 +0800

    alert box for index

---
+++ valid.php
@@ -0,0 +1,21 @@
+<!-- Alert Box -->
+
+<script src="js/sweetalert.min.js"></script>
+
+<?php
+
+if (isset($_SESSION['status']) && $_SESSION['status'] !='') {
+    ?>
+    <script>
+        swal({
+            title: "<?php echo $_SESSION['status']; ?>", 
+            icon: "<?php echo $_SESSION['status_code']; ?>",
+            button: "Confirm",
+
+        });
+    </script>
+    <?php
+        unset($_SESSION['status']);
+}
+
+?>
